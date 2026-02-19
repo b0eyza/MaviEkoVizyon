a9b1c3e9581bc0cba4f173d1df7118637ae09776
b0eyza <mai.lamia.66@gmail.com>
2026-02-19 15:16:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,3 +19,14 @@ with open("index.html", "r", encoding="utf-8") as f:
 
 # Tam ekran yüksekliği veriyoruz (Apple hissi için)
 components.html(html_code, height=1500, scrolling=False)
+import requests
+
+def get_weather():
+    url = "https://api.open-meteo.com/v1/forecast?latitude=40.15&longitude=26.40&daily=temperature_2m_max,sunshine_duration&timezone=auto"
+    response = requests.get(url)
+    data = response.json()
+    
+    temp = data["daily"]["temperature_2m_max"][0]
+    sun = data["daily"]["sunshine_duration"][0] / 3600  # saniyeden saate
+    
+    return temp, sun
